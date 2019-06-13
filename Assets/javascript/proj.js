764cbebd3e39ad104b8bd0e573b5821c7ca537a4
jsf5077 <jsf5077@gmail.com>
2019-06-12 21:58:59 -0400

    ingredients added get pushed into an array. The correct syntax has been found in order to do multiple ingredient searches.

--- Assets/javascript/proj.js
+++ Assets/javascript/proj.js
@@ -12,12 +12,12 @@
 // // Create a variable to reference the database
 // var db = firebase.database();
 
-var queryURL = `https://api.edamam.com/search?q=chicken&app_id=cd0febb4&app_key=6d1400c54461c5fb357e208675e77e00&from=0&to=3&calories=591-722&health=alcohol-free`;
+var queryURL = "https://api.edamam.com/search?q=chicken%2Crice&app_id=cd0febb4&app_key=6d1400c54461c5fb357e208675e77e00&from=0&to=3&calories=591-722&health=alcohol-free";
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  $("#foodData").text(JSON.stringify(response));
+  results.text(JSON.stringify(response));
 });
 
 $("#submitIngredient").on("click", function(event) {
@@ -25,6 +25,17 @@ $("#submitIngredient").on("click", function(event) {
     var ingredient = $("#ingredient-input").val().trim();
     $("#ingredientList").append( 
         "<li class='list-group-item'>"+ingredient+"</li>");
-
+    list.push(ingredient);
+    console.log(list);  
 });
+
+var list = [];
+
+var results = "";
+
+$("#check-recipe").on("click", function(event) {
+    event.preventDefault();
+    console.log(results);  
+});
+
 
