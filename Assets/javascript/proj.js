c2293dde9296ae543da0add61ae1924557468126
jsf5077 <jsf5077@gmail.com>
2019-06-12 20:30:57 -0400

    Updates for index including ingredient form and list. js has working food API. Database needs work

--- Assets/javascript/proj.js
+++ Assets/javascript/proj.js
@@ -1,9 +1,30 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBBDgBqGMtIDNTScDk9-LajgqBSOTkURb0",
-  authDomain: "food-wine-project.firebaseapp.com",
-  databaseURL: "https://food-wine-project.firebaseio.com",
-  projectId: "food-wine-project",
-  storageBucket: "food-wine-project.appspot.com",
-  messagingSenderId: "760442689212",
-  appId: "1:760442689212:web:7b64254f6a203455"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBBDgBqGMtIDNTScDk9-LajgqBSOTkURb0",
+//   authDomain: "food-wine-project.firebaseapp.com",
+//   databaseURL: "https://food-wine-project.firebaseio.com",
+//   projectId: "food-wine-project",
+//   storageBucket: "food-wine-project.appspot.com",
+//   messagingSenderId: "760442689212",
+//   appId: "1:760442689212:web:7b64254f6a203455"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// // Create a variable to reference the database
+// var db = firebase.database();
+
+var queryURL = `https://api.edamam.com/search?q=chicken&app_id=cd0febb4&app_key=6d1400c54461c5fb357e208675e77e00&from=0&to=3&calories=591-722&health=alcohol-free`;
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  $("#foodData").text(JSON.stringify(response));
+});
+
+$("#submitIngredient").on("click", function(event) {
+    event.preventDefault();
+    var ingredient = $("#ingredient-input").val().trim();
+    $("#ingredientList").append( 
+        "<li class='list-group-item'>"+ingredient+"</li>");
+
+});
+
