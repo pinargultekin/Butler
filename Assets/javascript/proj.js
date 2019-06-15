5faa4c09c7bf512451b7b2a975fe1c2b6511197b
jsf5077 <jsf5077@gmail.com>
2019-06-15 14:13:09 -0400

    can pull results back from food API and display on second page

--- Assets/javascript/proj.js
+++ Assets/javascript/proj.js
@@ -21,14 +21,31 @@
 var resultsDummy = "dummy results";
 
 var output = "";
+var itemSearch = "";
+// var queryURL = "https://api.edamam.com/search?q="+itemSearch+"&app_id=cd0febb4&app_key=6d1400c54461c5fb357e208675e77e00&from=0&to=3";
 
-var recipeResults = function(recipe) {
+var recipeResults = function() {
   var queryURL = "https://api.edamam.com/search?q="+itemSearch+"&app_id=cd0febb4&app_key=6d1400c54461c5fb357e208675e77e00&from=0&to=3";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    
+
+    var rResults = response.hits;
+
+    console.log(rResults);
+
+    for (var j = 0; j < rResults.length; j++) {
+      rdiv = $("<div>");
+      var img = $("<img class='image'>");
+      img.attr("src", rResults[j].recipe.image);
+      var name = $("<div>");
+      name.prepend(rResults[j].recipe.label);
+      rdiv.prepend(img);
+      $("#rec-demo").prepend(rdiv, name);
+    }
+
+
   });
 };
 
@@ -38,7 +55,8 @@ $("#submitIngredient").on("click", function(event) {
     $("#ingredientList").append( 
         "<li class='list-group-item'>"+ingredient+"</li>");
     list.push(ingredient);
-    // console.log(list);  
+    console.log(list); 
+    $("#ingredient-input").val("");
 });
 
 var list = [];
@@ -48,18 +66,22 @@ var display = [list.length];
 
 $("#recipe-check").on("click", function(event) {
   event.preventDefault();
-  for (i=1; i < list.length; i++) {
+  for (i=0; i < list.length; i++) {
     console.log(list[i]);
     
     output = output + "%2C" + list[i];
   }
   display = output;
-  var itemSearch =  list[0] + display;
-  console.log(itemSearch);
+  itemSearch =  list[0] + display;
+  
   $("#page1").hide();
   $("#page2").show();
-  
-    
+  recipeResults();
+});
+
+$(document).ready(function(){
+  $("#page1").show();
+  $("#page2").hide();
 });
 
 
